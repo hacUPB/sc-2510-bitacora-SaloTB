788de2fd762967d129a00d91a88abd0d94ddf37d
B09S319est <B09S319est@MEDAULAS.UPB>
2025-01-27 17:21:34 -0500

    Punto 10

--- Unidad 1/Actividad 5.asm
+++ Unidad 1/Actividad 5.asm
@@ -68,4 +68,14 @@ Optimizacion de:
 @i
 M=M+1
 
-#10 
+#10 Las posiciones de memoria RAM de 0 a 15 tienen los nombres simbólico R0 a R15. Escribe un programa en lenguaje ensamblador que guarde en R1 la operación 2 * R0
+@0
+D=M 
+@4 
+M=D 
+@4
+D=D+M
+@1
+M=D
+
+#11 
