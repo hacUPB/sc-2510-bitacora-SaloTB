6280385d1414cb7346f7bd3f9e556a9f8fd4fdb9
B09S319est <B09S319est@MEDAULAS.UPB>
2025-01-27 17:14:21 -0500

    Hasta el 9

--- Unidad 1/Actividad 5.asm
+++ Unidad 1/Actividad 5.asm
@@ -1,24 +1,28 @@
-# Carga en D el valor 1978.
+#1 Carga en D el valor 1978.
 @1978
 D=A
-# Guarda en la posición 100 de la RAM el número 69.
+
+#2 Guarda en la posición 100 de la RAM el número 69.
 @69
 D=A
 @100
 M=D
-# Guarda en la posición 200 de la RAM el contenido de la posición 24 de la RAM
+
+#3 Guarda en la posición 200 de la RAM el contenido de la posición 24 de la RAM
 @24
 D=M
 @200
 M=D 
-# Lee lo que hay en la posición 100 de la RAM, resta 15 y guarda el resultado en la posición 100 de la RAM
+
+#4 Lee lo que hay en la posición 100 de la RAM, resta 15 y guarda el resultado en la posición 100 de la RAM
 @100
 D=M
 @15
 D=D-A
 @100
 M=D
-# Suma el contenido de la posición 0 de la RAM, el contenido de la posición 1 de la RAM y con la constante 69. Guarda el resultado en la posición 2 de la RAM.
+
+#5 Suma el contenido de la posición 0 de la RAM, el contenido de la posición 1 de la RAM y con la constante 69. Guarda el resultado en la posición 2 de la RAM.
 @0
 D=M 
 @1
@@ -27,14 +31,41 @@ D=D+M
 D=D+A
 @2
 M=D
-# Si el valor almacenado en D es igual a 0 salta a la posición 100 de la ROM.
+
+#6 Si el valor almacenado en D es igual a 0 salta a la posición 100 de la ROM.
 @100
 D;JEQ
-# Si el valor almacenado en la posición 100 de la RAM es menor a 100 salta a la posición 20 de la ROM.
+
+#7 Si el valor almacenado en la posición 100 de la RAM es menor a 100 salta a la posición 20 de la ROM.
 @100
 D=M 
 @100
 D=D-A 
 @20
 D;JLT 
-# 
+
+#8 Consideracion de porgrama
+"@var1
+D = M
+@var2
+D = D + M
+@var3
+M = D"
+Este programa suma el valor almacenado en la RAM de dos puntos aleatorios llamados var1 y var2, y mueve el resultado de esta suma a la posicion var3 en la RAM 
+var1, var2 y var3 se encuentran en las posiciones 16, 17 y 18 correspodientemente, esto pues las posiciones de la 0 a la 15 son espcios reservados de la RAM 
+
+#9 Consideracion del programa
+"i = 1
+sum = 0
+sum = sum + i
+i = i + 1"
+Este codigo suma la cantidad existente en la RAM de la primera variable en la segunda 
+i y sum son las variables 16 y 17 respectivamente por la misma razon anterior.
+
+Optimizacion de:
+
+"// i = i + 1"
+@i
+M=M+1
+
+#10 
